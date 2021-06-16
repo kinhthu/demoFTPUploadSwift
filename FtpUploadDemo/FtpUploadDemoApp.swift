83bbdbdc0d985d323077e367d6b0b60620ffa552
Thu Le <thu.le@launchdeck.org>
2021-06-16 17:46:00 +0700

    Initial Commit

---
+++ FtpUploadDemo/FtpUploadDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FtpUploadDemoApp.swift
+//  FtpUploadDemo
+//
+//  Created by Kinh Thu on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct FtpUploadDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
